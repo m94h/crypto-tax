d495974eeeb52f2745c19830ff6f48c07ef6d475
mihurtado <matias@fintual.com>
2024-04-05 18:16:32 -0300

    fix admin user admin

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,6 +5,10 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["email"]
+  end
 end
 
 # == Schema Information
